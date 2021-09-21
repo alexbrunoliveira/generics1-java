336d00a045b787c257c23f3d002a3ca3f6bba801
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com>
2021-09-21 00:25:07 -0300

    Add files via upload

---
+++ PrintService.java
@@ -0,0 +1,34 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+//  Generics permitem que classes, interfaces e métodos
+//Reuso  , Type safety,  Performance
+
+public class PrintService<T> {
+
+	private List<T> list = new ArrayList<>();
+
+	public void addValue(T value) {
+		list.add(value);
+	}
+	// Pode ser usada qualquer letra para definir o tipo
+	
+	public T first() {
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		}
+		return list.get(0);
+	}
+
+	public void print() {
+		System.out.print("[");
+		if (!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.println("]");
+	}
+}
